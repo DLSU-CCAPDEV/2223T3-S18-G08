0e3157cfce8563a051947f9d9280a1974f458e08
LuAndre49 <andre_giancarlo_lu@dlsu.edu.ph>
2023-06-20 16:45:09 +0800

    ver3

--- scripts.js
+++ scripts.js
@@ -1,8 +1,22 @@
 function checkPassword(){
     if(document.getElementById("password").value == document.getElementById("confirmpassword").value){
+        document.write();
         document.location.href = "index.html";
     } else {
         alert("Password and confirm password do not match!");
+    }
+}
+
+function checkLogin(){
+    if((document.getElementById("email").value == "arren.antioquia@dlsu.edu.ph" && document.getElementById("password").value == "CCAPDEV_prof") ||
+       (document.getElementById("email").value == "andre_giancarlo_lu@dlsu.edu.ph" && document.getElementById("password").value == "AGL") ||
+       (document.getElementById("email").value == "adriel_donato@dlsu.edu.ph" && document.getElementById("password").value == "AJ123") ||
+       (document.getElementById("email").value == "adriel_shanlley_teng@dlsu.edu.ph" && document.getElementById("password").value == "lolafobic") ||
+       (document.getElementById("email").value == "ethan_lester_l_chan@dlsu.edu.ph" && document.getElementById("password").value == "ELC1123")){
+        document.write();
+        document.location.href = "profile.html";
+    } else {
+        alert("Email or password is incorrect!");
     }
 }
 
