74ac8328ab8c53dc960fcd0ca2363b8ee3df6442
LuAndre49 <andre_giancarlo_lu@dlsu.edu.ph>
2023-06-21 00:51:44 +0800

    added base html files for the other features added reservation functionality added links to each feature in the profile page

--- scripts.js
+++ scripts.js
@@ -22,29 +22,51 @@ function checkLogin(){
 
 var slots = new Array();
 
-function makeArray(d, h, w) {
-    for(i=0;i<d;i++){
+function makeArray(l, d, t, s) {
+    for(i=0;i<l;i++){
         slots[i] = new Array();
-        for(j=0;j<h;j++){
+        for(j=0;j<d;j++){
             slots[i][j] = new Array();
-            for(k=0;k<w;k++){
-                slots[i][j][k] = Math.floor(Math.random() * 2);
+            for(k=0;k<t;k++){
+                slots[i][j][k] = new Array();
+                for(n=0;n<s;n++){
+                    slots[i][j][k][n] = Math.floor(Math.random() * 2);
+                }
             }
         }
     }
 }
 
 function displaySlots(){
-    var displayslots = slots[document.getElementById("lab_num").value-1][document.getElementById("day_num").value];
+    var displayslots = slots[document.getElementById("lab_num").value-1][document.getElementById("day_num").value][document.getElementById("time").value];
     table = document.getElementById("slots");
    
     for(i = 0; i < 20; i++){
         if(displayslots[i] == 0){
-            table.rows[Math.floor(i/5)].cells[i%5].innerHTML = "Free";
+            table.rows[Math.floor(i/5)].cells[i%5].innerHTML = "Seat " + (i+1) + "(Free)";
         } else {
-            table.rows[Math.floor(i/5)].cells[i%5].innerHTML = "Taken";
+            table.rows[Math.floor(i/5)].cells[i%5].innerHTML = "Seat " + (i+1) + "(Taken)";
         }
     }
     
     document.getElementById("slots").style.display = "block";
 }
+
+function displayFreeSlots(){
+    var displayslots = slots[document.getElementById("search_lab_num").value-1][document.getElementById("search_day_num").value][document.getElementById("search_time").value];
+    var freeslots = "Free slots: ";
+    for(i = 0; i < 20; i++){
+        if(displayslots[i] == 0){
+            freeslots = freeslots + "[Seat " + (i+1) + "] ";
+        }
+    }
+
+    document.getElementById("freeslots").innerHTML = freeslots;
+}
+
+function checkFree(){
+    /* checks to make sure the specific seat is indeed free, in which case link them back to profile.html
+        else alert that the slot is already taken
+    */
+}
+
