d85bfacf2f7ea6d2ffe63b4a3063dc3683f350b9
LuAndre49 <andre_giancarlo_lu@dlsu.edu.ph>
2023-06-20 17:28:23 +0800

    ver4

--- scripts.js
+++ scripts.js
@@ -20,3 +20,31 @@ function checkLogin(){
     }
 }
 
+var slots = [0, 1, 0][0, 1, 0, 0, 0, 1, 0, 1][0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+
+var slots = [0, 1, 2][0, 1, 2, 3, 4, 5, 6, 7][0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+/*
+for (i = 0; i < 3; i++){
+    for(j = 0; j < 8; j++){
+        for(k = 0; k < 20; k++){
+            slots[i][j].append(Math.floor(Math.random() * 2));
+        }
+    }
+}
+*/
+function displaySlots(labnumber, daynumber){
+    displayslots = slots[labnumber-1][daynumber];
+    table = document.getElementById("slots");
+    /*
+    for(i = 0; i < 20; i++){
+        if(displayslots[i] == 0){
+            table[i].innerHTML = "Free";
+        } else {
+            table[i].innerHTML = "Taken";
+        }
+    }
+    */
+    
+    document.getElementById("slots").style.display = "block";
+
+}
